af70bedb4cc5c2a351eedaf146a216d8e247270b
Thiha Soe <thihasoe101100@gmail.com>
2024-11-06 10:58:46 +0800

    Modifed bmi.py to return integer

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,42 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+
+def display_main_menu():
+    print("display_main-menu")
+    print("Enter some numbers separated by commas (e.g. 5,67,32)")
+
+def get_user_input():
+    print("get_user_input")
+    inputstr = input()
+    print("Raw Input = " + inputstr)
+
+    #split the input string into segments of strings using comma as spliiter
+    splitlist = inputstr.split(",") 
+    print("After splitting = ", splitlist)
+
+    #Next step is to convert each short string in the list into float
+    floatlist = [] #define an empty list of float numbers
+    for x in splitlist:
+        floatnum = float(x)  #Convert string into float
+        floatlist.append(floatnum)  #Add the float to the floatlist
+    print("floatlist = ", floatlist)    
+    return floatlist
+
+def cal_average(input_list):
+    print("calc_average")
+
+def find_min_max(input_list):
+    print("find_min_max")
+
+def sort_temperature(input_list):
+    print("sort_temperature")
+
+def calc_median_temperature(input_list):
+    print("calc_median_temperature")
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    floatlist = get_user_input()  
+
+if __name__ == "__main__":
+    main()
--- bmi.py
+++ bmi.py
@@ -8,14 +8,17 @@ def calculate_bmi(height, weight):
     print("Weight Classification: ", end="")
     if BMI<18.5:
         print("Underweight")
+        return -1
     elif BMI>25.0:
         print("Overweight")
+        return 1
     else:
-        print("Normal weight")        
+        print("Normal weight") 
+        return 0       
 
-calculate_bmi(weight=57, height=1.73)
-print("=====================")
-calculate_bmi(weight=97, height=1.73)
-print("=====================")
-calculate_bmi(weight=37, height=1.73)
-print("=====================")
+result = calculate_bmi(weight=57, height=1.73)
+print("result = ", result)
+result = calculate_bmi(weight=97, height=1.73)
+print("result = ", result)
+result = calculate_bmi(weight=37, height=1.73)
+print("result = ", result)
